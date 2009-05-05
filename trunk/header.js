d80de59e1d49dbea38b506f376c91c83c26555a6
jon.t.moore <jon.t.moore@915c99e4-3862-11de-96ab-f16d9912eac9>
2009-05-05 04:20:21 +0000

    Now works on /build.php*.  When not enough resources to build something, indicates when there will be enough given current resources and production rates.

--- trunk/header.js
+++ trunk/header.js
@@ -3,4 +3,5 @@
 // @namespace http://www.fishwife.org/
 // @description Travian helper info script
 // @include http://s8.travian.us/dorf1.php*
+// @include http://s8.travian.us/build.php*
 // ==/UserScript==
